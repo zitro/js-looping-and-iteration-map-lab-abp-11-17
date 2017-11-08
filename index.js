e970f14ba806331576b536969351b3b8c655ac66
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 19:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function lowerCaseDrivers(array){
   });
 }
 
-/*function nameToAttributes(array){
-  let array3 = [];
-  return array.map(function(array3){
-  '${firstName} ${lastName}'
-}} */
+function nameToAttributes(array) {
+  var newObj = {};
+  for (var i = 0; i < array.length; ++i)
+    newObj[i] = array[i];
+  return newObj;
+}
 
 /*this function takes an array of drivers, and returns
 an array of the drivers names in lowercase.*/
