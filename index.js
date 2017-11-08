98f9c9b17f1b7b720e3755d3ed93480283a3a88a
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-08 18:34:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+function lowerCaweDrivers(array){
+  let array2 = [];
+  return array.map(function(array2){
+    return array2.toLowerCase();
+  });
+}
+
+/*this function takes an array of drivers, and returns 
+an array of the drivers names in lowercase.*/
